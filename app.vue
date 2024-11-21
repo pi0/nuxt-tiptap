3dfa179c335c114a499409a8fd4a31b5004b4868
Sébastien Chopin <seb@nuxtlabs.com>
2024-11-21 11:26:20 +0100

    fix: use z-index of 10

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-group">
-    <div id="links" class="fixed top-4 right-5 underline z-1">
+    <div id="links" class="fixed top-4 right-5 underline z-10">
       <a href="https://github.com/pi0/nuxt-tiptap" target="_blank" rel="noopener noreferrer">view source on Github</a>
     </div>
     <client-only>
